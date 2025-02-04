4b591b88005f82c0c2008690902d55810780cc65
ziffa <rforkaliuk@gmail.com>
2025-02-04 12:21:27 +0200

    Add project files.

---
+++ cw_04_02_2025/cw_04_02_2025.cpp
@@ -0,0 +1,53 @@
+// определение символической константы _UNICODE
+#define _CRT_SECURE_NO_WARNINGS
+#define _UNICODE
+
+#include <iostream>
+#include <windows.h>
+#include <tchar.h>// дл€ макросов windows
+
+//  1. ѕользователь вводит текст(только кириллица).ѕодсчитать количество слов, которые заканчиваютс€ на гласную букву.
+
+using namespace std;
+
+int vowelEndingWordCount(_TCHAR str[])
+{
+    _TCHAR vow[] = _T("аеЄиоуыэю€ј≈®»ќ”џЁёя");
+
+    int vLen = _tcslen(vow);
+
+    auto isVowel = [&](_TCHAR ch)
+    {
+        for (int i = 0; i < vLen; i++)
+        {
+            if (ch == vow[i])
+            {
+                return true;
+            }
+        }
+        return false;
+    };
+
+    int count = 0;
+    int len = _tcslen(str);
+
+    for (int i = 0; i < len; i++)
+    {
+        if (isVowel(str[i]) && !_istalpha(str[i + 1]))
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+
+void main()
+{
+    SetConsoleOutputCP(65001);
+    SetConsoleCP(65001);
+
+    _TCHAR szBuf3[300] = _T("съешь ещЄ этих м€гких французских булок, да выпей чаю.");
+
+    wcout << vowelEndingWordCount(szBuf3) << endl;
+}
